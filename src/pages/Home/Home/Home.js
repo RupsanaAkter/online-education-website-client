44660af7bbd2d319d17d5bda735beac231519cb3
R7558 <rupsanaworld1004@gmail.co>
2023-05-18 01:05:35 +0600

    added review

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import InfoCards from '../InfoCard/InfoCards';
 import Services from '../Service/Services';
+import Reviews from '../Review/Reviews';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <InfoCards></InfoCards>
             <Services></Services>
+            <Reviews></Reviews>
         </div>
     );
 };
